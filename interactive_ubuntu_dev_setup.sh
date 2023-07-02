e8b4e80691ae1d129ebafe36d8484b08a3803115
Jose Maria CLi <chema9703@gmail.com>
2023-07-02 01:27:10 -0600

    Allow navigation with arrow keys and implement actual install functions
    This time the functions will perform actual package installation, and the user will be able to choose the packages they want to install with the arrow keys

--- interactive_ubuntu_dev_setup.sh
+++ interactive_ubuntu_dev_setup.sh
@@ -1,110 +1,54 @@
 #!/bin/bash
-  echo "This is Function 3."
-}
 
-# Interactive prompt to select functions
-# Interactive prompt to select functions
-# Interactive prompt to select functions
-# Global list of available functions
-declare -a available_functions=("function1" "function2" "function3")
-
-# Global array to store selected functions
-declare -a selected_functions=()
-
-# Interactive prompt to select functions
-function_prompt() {
-  local cursor=0
-  local options=("Option 1" "Option 2" "Option 3")
-
-  # Function to display checkboxes
-  display_checkboxes() {
-    for ((i = 0; i < ${#options[@]}; i++)); do
-      local checkbox="[ ]"
-      if [ $i -eq $cursor ]; then
-        checkbox=" >"
-      fi
-
-      if [[ " ${selected_functions[@]} " =~ " ${options[i]} " ]]; then
-        checkbox="[${checkbox}*]"
-      else
-        checkbox="[${checkbox}]"
-      fi
-
-      echo "$checkbox ${options[i]}"
-    done
-  }
-
-  # Main loop
-  while true; do
-    clear
-    echo "Select functions to execute: (use 'q' to navigate up, 'a' to navigate down, 'y' to toggle, press Enter to confirm)"
-
-    display_checkboxes
-
-    # Read user input
-    read -rsn1 input
-
-    case $input in
-    'q' | 'Q')
-      # "q" or "Q" key: move cursor up
-      cursor=$(( (cursor - 1 + ${#options[@]}) % ${#options[@]} ))
-      ;;
-    'a' | 'A')
-      # "a" or "A" key: move cursor down
-      cursor=$(( (cursor + 1) % ${#options[@]} ))
-      ;;
-    'y' | 'Y')
-      # Toggle checkbox state if the input is 'y' or 'Y'
-      local selected_option=${options[cursor]}
-      if [[ " ${selected_functions[@]} " =~ " $selected_option " ]]; then
-        selected_functions=("${selected_functions[@]/$selected_option}")
-      else
-        selected_functions+=("$selected_option")
-      fi
-      ;;
-    '')
-      # Break the loop if the input is Enter (confirm selection)
-      break
-      ;;
-    esac
-  done
-}
-
-# Prompt the user to select functions
-array_name=("string1" "string2" "string3")
-array_name+=("string4", "string5")
-
-echo "function_prompt loaded"
-
-# Execute selected functions
-function_prompt
-
-echo "Selected functions:"
-for function in "${selected_functions[@]}"; do
-  echo "- $function"
-done
-#!/bin/bash
+# Check if the script is run as root (with sudo)
+if [[ $(id -u) -ne 0 ]]; then
+  echo "Please run this script with sudo or as root."
+  exit 1
+fi
 
 # Function 1
-function function1() {
-  echo "This is Function 1."
+function installZSH() {
+  echo "Installing ZSH..."
+
+  # Install Zsh
+  apt install -y zsh
+
+  # Change the default shell to Zsh
+  chsh -s /usr/bin/zsh
+
+  # Print installation completion message
+  echo "Zsh has been installed successfully."
 }
 
 # Function 2
-function function2() {
-  echo "This is Function 2."
-}
+function installTabby() {
+  echo "Installing Tabby..."
+  # Ask the user for the Tabby version
+  read -p "Enter the Tabby version to install (default: 1.0.197): " TABBY_VERSION
+  TABBY_VERSION=${TABBY_VERSION:-1.0.197}
 
-# Function 3
-function function3() {
-  echo "This is Function 3."
+  # Download Tabby
+  # Tabby is not part of the official Ubuntu repositories, so we need to download and install it manually
+
+  # Download Tabby (change the URL if needed)
+  TABBYY_FILE="tabby-${TABBY_VERSION}-linux-x64.deb"
+  TABBYY_DOWNLOAD_URL="https://github.com/Eugeny/tabby/releases/download/v${TABBY_VERSION}/${TABBYY_FILE}"
+  wget "$TABBYY_DOWNLOAD_URL" -O /tmp/"$TABBYY_FILE"
+
+  # Install Tabby
+  dpkg -i /tmp/"$TABBYY_FILE"
+
+  # Clean up temporary files
+  rm /tmp/"$TABBYY_FILE"
+
+  # Print installation completion message
+  echo "Zsh and Tabby have been installed successfully."
 }
 
 # Interactive prompt to select functions
 # Interactive prompt to select functions
 # Interactive prompt to select functions
 # Global list of available functions
-declare -a available_functions=(function1 function2 function3)
 
 # Global array to store selected functions
 declare -a selected_functions=()
@@ -112,20 +56,20 @@ declare -a selected_functions=()
 # Interactive prompt to select functions
 function_prompt() {
   local cursor=0
-  local options=("Function 1" "Function 2" "Function 3")
+  local options=("ZSH" "Tabby")
 
   # Function to display checkboxes
   display_checkboxes() {
     for ((i = 0; i < ${#options[@]}; i++)); do
-      local checkbox="[ ]"
+      local checkbox="  "
       if [ $i -eq $cursor ]; then
         checkbox=" >"
       fi
 
       if [[ " ${selected_functions[@]} " =~ " $i " ]]; then
-        checkbox="[${checkbox}*]"
+        checkbox="[*] ${checkbox}"
       else
-        checkbox="[${checkbox}]"
+        checkbox="[ ] ${checkbox}"
       fi
 
       echo "$checkbox ${options[i]}"
@@ -135,7 +79,7 @@ function_prompt() {
   # Main loop
   while true; do
     clear
-    echo "Select functions to execute: (use 'q' to navigate up, 'a' to navigate down, 'y' to toggle, press Enter to confirm)"
+    echo "What to you want to install? (use 'q' to navigate up, 'a' to navigate down, 'y' to toggle, press Enter to confirm)"
 
     display_checkboxes
 
@@ -143,15 +87,15 @@ function_prompt() {
     read -rsn1 input
 
     case $input in
-    'q' | 'Q')
+    'a' | 'A')
       # "q" or "Q" key: move cursor up
       cursor=$(( (cursor - 1 + ${#options[@]}) % ${#options[@]} ))
       ;;
-    'a' | 'A')
+    'b' | 'B')
       # "a" or "A" key: move cursor down
       cursor=$(( (cursor + 1) % ${#options[@]} ))
       ;;
-    'y' | 'Y')
+    'c' | 'C' | 'd' | 'D')
       # Toggle checkbox state if the input is 'y' or 'Y'
       local selected_index=$cursor
       if [[ " ${selected_functions[@]} " =~ " $selected_index " ]]; then
@@ -168,29 +112,22 @@ function_prompt() {
   done
 }
 
-# Prompt the user to select functions
-array_name=("string1" "string2" "string3")
-array_name+=("string4", "string5")
-
-echo "function_prompt loaded"
-
 # Execute selected functions
 function_prompt
+
+apt update
+apt install -y wget curl
 
 echo "Executing selected functions:"
 for index in "${selected_functions[@]}"; do
   case $index in
   0)
-    function1
+    installZSH
     ;;
   1)
-    function2
-    ;;
-  2)
-    function3
+    installTabby
     ;;
   esac
+  sudo apt --fix-broken install
+  sudo apt autoremove
 done
-
-# all right, my bad. Discard this last change.
-# Now, using the previous version of the script, instead of saving the selected option string to the selected_functions array, I need to store the position of the selected option only
